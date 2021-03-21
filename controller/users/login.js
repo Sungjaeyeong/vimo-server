5b055be5181cd13f6fd0c9aba730d8193b1467b3
spark0109 <seonabak0109@gmail.com>
2021-03-21 14:54:44 +0900

    Add feature4

--- controller/users/login.js
+++ controller/users/login.js
@@ -1,5 +1,14 @@
+const { users } = require('../../models')
 module.exports = {
-  post: (req, res) => {
-    res.json({data: "hi"})
+  post: async (req, res) => {
+    const userInfo = await users.findOne({
+      where: { email: req.body.email, password: req.body.password }
+    })
+    if (userInfo) {
+      req.session.userId = userInfo.id;
+      res.status(200).send({data: userInfo})
+    } else {
+      res.status(404).send('no')
+    }
   },
 };
