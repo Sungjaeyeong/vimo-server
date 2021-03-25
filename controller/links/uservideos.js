f4af11e1a9954e3c061cace221748601a977038b
성재영 <jaeyeong@MacBookPro.local>
2021-03-25 16:20:27 +0900

    Fix10

--- controller/links/uservideos.js
+++ controller/links/uservideos.js
@@ -9,7 +9,7 @@ module.exports = {
       res.json({ data: null, message: "invalid access token" })
     } else {
       const token = authorization.split(' ')[1];
-      if (token !== '' || !token) {
+      if (token !== '' && !token) {
         stateData = JWT.verify(token, process.env.ACCESS_SECRET);
       }
       data = stateData
